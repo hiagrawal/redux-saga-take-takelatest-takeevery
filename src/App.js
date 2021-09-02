c6b406b0983860ce842016af2a9d7d4d65513daa
hiagrawal <agrawal.hina13@gmail.com>
2021-09-02 13:48:14 +0530

    example of userReducer hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import SagasExample from './components/sagas-example/sagas-example.component';
+import UseStateExample from './components/use-state-example/use-state-example.component';
+import UseEffectExample from './components/use-effect-example/use-effect-example.component';
+import UseReducerExample from './components/use-reducer-example/use-reducer-example.component';
+
 
 import './App.css';
 
--- src/components/use-reducer-example/use-reducer.component.jsx
+++ src/components/use-reducer-example/use-reducer.component.jsx
@@ -1,23 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 
 import Card from '../card/card.component';
 
+//useReducer is used for local state like useState
+//only thing is if we have complex states then instead to useState, it is preferred to use useReducer
+//useReducer works on the same principle as of redux and gives us action, reducer functions, state and dispatch
+
+const INITIAL_STATE = {
+  currentUser: null,
+  searchQuery: 'Bret'
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        currentUser: action.payload
+      };
+    case 'SET_SEARCH_QUERY':
+      return {
+        ...state,
+        searchQuery: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+const setUser = user => ({
+  type: 'SET_USER',
+  payload: user
+});
+
+const setSearchQuery = queryString => ({
+  type: 'SET_SEARCH_QUERY',
+  payload: queryString
+});
+
+//userEducer takes first paramter the reducer function like we had reducer in redux and 
+//second paramter as initial state same as use used to set in reducer
+//and returns us the state and dispatch property to use state and action functions
 const UseReducerExample = () => {
-  const [user, setUser] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+  const { user, searchQuery } = state;
 
   useEffect(() => {
-    if (searchQuery.length > 0) {
-      const fetchFunc = async () => {
-        const response = await fetch(
-          `https://jsonplaceholder.typicode.com/users?username=${searchQuery}`
-        );
-        const resJson = await response.json();
-        setUser(resJson[0]);
-      };
+    const fetchFunc = async () => {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users?username=${searchQuery}`
+      );
+      const resJson = await response.json();
+      dispatch(setUser(resJson[0]));
+    };
 
-      fetchFunc();
-    }
+    fetchFunc();
   }, [searchQuery]);
 
   return (
@@ -25,7 +62,7 @@ const UseReducerExample = () => {
       <input
         type='search'
         value={searchQuery}
-        onChange={event => setSearchQuery(event.target.value)}
+        onChange={event => dispatch(setSearchQuery(event.target.value))}
       />
       {user ? (
         <div>
